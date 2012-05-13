8cd5045a20200218fa20fa1a702d029b64f7be7b
Henri Verroken <henriverroken@gmail.com>
2012-05-13 14:51:18 +0000

    Added documention for CSXXMLWriting.

--- Classes/CSXXMLWriter.h
+++ Classes/CSXXMLWriter.h
@@ -38,6 +38,50 @@
 
 #import "NSString+CSXLibXMLConversion.h"
 
+/*!
+ The class `CSXXMLWriter` is used to write an XML document with a predefined
+ layout.
+ 
+ Using a `CSXDocumentLayout` and an instance of your document class, this class
+ can write out an XML document to a file and to the memory.
+ 
+ #Errors
+ 
+ Errors are instances of `NSError`. The localized description and localized
+ recovery suggestion are set.
+ 
+ - Domains (`NSString`)
+    - `CSXXMLWriterErrorDomain`
+        This class uses this custom error domain.
+
+ - Error codes
+    - `kCSXXMLWriterTextWriterCreationError`
+ 
+        The writer can not create an instance of the LibXML text writer.
+ 
+    - `kCSXXMLWriterTextWriterWriteError`
+ 
+        An error occurred while trying to write with the LibXLM text writer
+        instance.
+ 
+ #Exceptions
+ 
+ - `CSXXMLWriterNoRootInstanceException`
+ 
+    Thrown when the property `rootInstance` is not set when a write method
+    is invoked.
+ 
+ - `CSXXMLWriterNoDocumentLayoutException`
+ 
+    Thrown when the property `documentLayout` is not set when a write method
+    is invoked.
+ 
+ - `CSXXMLWriterInvalidAttributeTypeException`
+ 
+    Thrown when trying to write an object's attribute which `CSXNodeContentLayout`
+    specifies a type not available for an attribute.
+ */
+
 extern NSString * const CSXXMLWriterErrorDomain;
 
 extern NSString * const CSXXMLWriterNoRootInstanceException;
@@ -58,21 +102,94 @@ enum {
     } _state;
 }
 /* MARK: Init */
+/*! @name Creating and Initializing */
+/*!
+ Initialize an instance with a given document layout.
+ 
+ @param layout A valid instance of `CSXDocumentLayout` representing the
+ layout of your XML document.
+ */
 - (id)initWithDocumentLayout:(CSXDocumentLayout *)layout;
+
+/*!
+ Returns the autoreleased result of initWithDocumentLayout:.
+ 
+ @see initWithDocumentLayout:
+ */
 + (id)XMLWriterWithDocumentLayout:(CSXDocumentLayout *)layout;
 
+/*!
+ Initialize an instance with the `CSXDocumentLayout` contained by the given
+ layout document.
+ 
+ Using this function is equivalent to calling 
+ [CSXDocumentLayout initWithLayoutDocument:error:] and than calling
+ initWithDocumentLayout:.
+ 
+ @see [CSXDocumentLayout initWithLayoutDocument:error:]
+ */
 - (id)initWithLayoutDocument:(NSString *)f error:(NSError **)err;
+
+/*!
+ Returns the autoreleased result of initWithLayoutDocument:error:.
+ 
+ @see initWithLayoutDocument:error:
+ */
 + (id)XMLWriterWithLayoutDocument:(NSString *)f error:(NSError **)err;
 
 /* MARK: Properties */
+/*! @name Properties */
+/*!
+ The document layout of your XML document.
+ */
 @property (nonatomic, retain) CSXDocumentLayout *documentLayout;
+
+/*!
+ An instance of your custom class containing the document content.
+ */
 @property (nonatomic, retain) id rootInstance;
 
+/*!
+ The XML version.
+ 
+ This defaults to the LibXML default.
+ */
 @property (nonatomic, retain) NSString *XMLVersion;
+
+/*!
+ The encoding of the file.
+ 
+ This defaults to the LibXML default.
+ */
 @property (nonatomic, retain) NSString *encoding;
+
+/*!
+ The compression.
+ 
+ This defaults to 0. See the LibXLM manual for more information.
+ */
 @property (nonatomic, assign) int compression;
 
 /* MARK: Writing */
+/*! @name Writing */
+/*!
+ Use this method to write the XML document to a file.
+ 
+ @param file The output file.
+ 
+ @param errptr A variable capable of holding the error if one occurs.
+ 
+ @return `YES` if the writing was successful or `NO` if an error occurred.
+ 
+ @exception CSXXMLWriterNoRootInstanceException Thrown when the property
+ `rootInstance` is not set.
+ 
+ @exception CSXXMLWriterNoDocumentLayoutException Thrown when the property
+ `documentLayout` is not set.
+ 
+ @exception CSXXMLWriterInvalidAttributeTypeException Thrown when an invalid
+ attribute type is met while writing out the document.
+ */
 - (BOOL)writeToFile:(NSString *)file error:(NSError **)errptr;
 @end
 
